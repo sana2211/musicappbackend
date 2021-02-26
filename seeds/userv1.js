0a890e95acd4f3c35e220fa15b7e967e4830e46f
Sana Siddiqui <sanafaizsiddiqui@me.com>
2021-02-26 22:56:03 +0400

    msg

---
+++ seeds/userv1.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {fullname: 'Jon Doe 1', email: 'jondoe1@email.com', password: 'password'},
+        {fullname: 'Jon Doe 2', email: 'jondoe2@email.com', password: 'password'},
+        {fullname: 'Jon Doe 3', email: 'jondoe3@email.com', password: 'password'},
+       
+      ]);
+    });
+};
