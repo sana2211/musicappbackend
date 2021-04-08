ef957ab2f21c2fe51fc3da66d2cbc79480d46f11
Sana Siddiqui <sanafaizsiddiqui@me.com>
2021-04-08 21:49:41 +0400

    msg

--- seeds/userv1.js
+++ seeds/userv1.js
@@ -1,14 +1,17 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  /*return knex('users').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {fullname: 'Jon Doe 1', email: 'jondoe1@email.com', password: 'password'},
-        {fullname: 'Jon Doe 2', email: 'jondoe2@email.com', password: 'password'},
-        {fullname: 'Jon Doe 3', email: 'jondoe3@email.com', password: 'password'},
+        {restaurantName: 'Restaurant 1', email: 'jondoe1@email.com', 
+        password: 'password', jobTitle: "Manager", address: 'Address1'},
+        {restaurantName: 'Restaurant2', email: 'jondoe2@email.com', 
+        password: 'password', jobTitle: "Manager", address: 'Address2'},
+        {restaurantName: 'Restaurant 3', email: 'jondoe3@email.com', 
+        password: 'password', jobTitle: "Manager", address: 'Address3'},
        
       ]);
-    });*/
+    });
 };
